9b5afc213ef9b873db2fd34074f7b1d5dbff96b5
KathanDesai <kathandesai@ymail.com>
2018-04-08 18:26:16 -0400

    First commit for API

---
+++ BNYLineageAPI/src/main/java/com/mycompany/bnylineageapi/Neo4JHandler.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.bnylineageapi;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Kathan
+ */
+public class Neo4JHandler {
+    public void addObject(JSONObject obj){
+        
+    }
+}
